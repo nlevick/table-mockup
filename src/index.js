a3e59af4e7d90bef820aa2c7b4cf867a0fdd6d42
Nathan Levick <nlevick@indicalab.com>
2022-01-26 17:13:11 -0700

    Rename component to components again

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { ThemeProvider } from "styled-components";
 
-import App from "./component/App";
+import App from "./components/App";
 import theme from "./style/theme";
 import GlobalStyle from "./style";
 
